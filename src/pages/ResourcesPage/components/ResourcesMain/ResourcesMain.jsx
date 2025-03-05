c65484686d18792a0f144c585e0e37249dd60d15
Vchen7629 <chenvincent7629@gmail.com>
2025-03-05 01:31:08 -0800

    added animations + made it mobile responsive

--- src/pages/ResourcesPage/components/ResourcesMain/ResourcesMain.jsx
+++ src/pages/ResourcesPage/components/ResourcesMain/ResourcesMain.jsx
@@ -1,24 +1,78 @@
 import styles from "./resources-main.module.css";
 
-import pixelTree from "@assets/pixeltree2.png"
+import pixelTree from "@assets/resourcespage/pixeltree2.png"
 
 import GameJamWorkshops from "../Workshops/gamejamworkshops";
 import PopularGameEngines from "../PopularGameEngines/popularGameEngines";
 import PopularArtTools from "../artTools/popularArtTools";
 import PopularSoundTools from "../soundTools/popularSoundTools";
-
+import { useState } from "react";
+import logs from "@assets/resourcespage/logs.png"
+import test from "@assets/resourcespage/test.png"
 
 const ResourcesBody = () => {
+    const [displayWorkshopInfo, setDisplayWorkshopInfo] = useState(false);
+    const [displayGameEngineInfo, setGameEngineInfo] = useState(false);
+    const [displaySoundToolsInfo, setSoundToolsInfo] = useState(false);
+    const [displayArtToolsInfo, setArtToolsInfo] = useState(false);
+
+    function handleExit() {
+        setDisplayWorkshopInfo(false);
+        setGameEngineInfo(false);
+        setSoundToolsInfo(false);
+        setArtToolsInfo(false);
+    }
+
+    const DisplayProps = {
+        setDisplayWorkshopInfo,
+        setGameEngineInfo,
+        setSoundToolsInfo,
+        setArtToolsInfo
+    }
+
     return (
         <section className={styles.resourcesMain}>
-            <h1 className={styles.resourcesTitle}>Resources</h1>
-            <div className={styles.bodyContainer}>
-                <GameJamWorkshops/>
-                <PopularGameEngines />
+            {displayWorkshopInfo ? (
+                <div className={styles.infocontainer}>
+                    <div className={styles.info}>
+
+                    </div>
+                    <button className={styles.button} onClick={handleExit}>X</button>
+                </div>
+            ) : displayGameEngineInfo ? (
+                <div className={styles.infocontainer}>
+                    <div className={styles.info}>
+
+                    </div>
+                    <button className={styles.button} onClick={handleExit}>X</button>
+                </div>
+            ) : displaySoundToolsInfo ? (
+                <div className={styles.infocontainer}>
+                    <div className={styles.info}>
+
+                    </div>
+                    <button className={styles.button} onClick={handleExit}>X</button>
+                </div>
+            ) :  displayArtToolsInfo && (
+                <div className={styles.infocontainer}>
+                    <div className={styles.info}>
+
+                    </div>
+                    <button className={styles.button} onClick={handleExit}>X</button>
+                </div>
+            )}
+            <div className={styles.resourcesTitleContainer}>
+                <img className={styles.logs} src={logs}/>
+                <h1 className={styles.resourcesTitle}>Resources</h1>
+                <img className={styles.logs} src={logs}/>
             </div>
             <div className={styles.bodyContainer}>
-                <PopularSoundTools />
-                <PopularArtTools />
+                <GameJamWorkshops DisplayProps={DisplayProps}/>
+                <PopularGameEngines DisplayProps={DisplayProps}/>
+            </div>
+            <div className={styles.bodyContainer}>
+                <PopularSoundTools DisplayProps={DisplayProps}/>
+                <PopularArtTools DisplayProps={DisplayProps}/>
             </div>
             <div className={styles.resourcesFooter}>
                 <img src={pixelTree} className={styles.img}/>
--- src/pages/ResourcesPage/components/Workshops/gamejamworkshops.jsx
+++ src/pages/ResourcesPage/components/Workshops/gamejamworkshops.jsx
@@ -1,12 +1,31 @@
 import styles from "./game-jam-workshops.module.css";
+import Hammer from "@assets/resourcespage/hammer.png"
 
-const GameJamWorkshops = () => {
+const GameJamWorkshops = ({ DisplayProps }) => {
+    const {
+        setDisplayWorkshopInfo,
+        setGameEngineInfo,
+        setSoundToolsInfo,
+        setArtToolsInfo
+    } = DisplayProps
+
+    function handleClick() {
+        setDisplayWorkshopInfo(true)
+        setGameEngineInfo(false)
+        setSoundToolsInfo(false)
+        setArtToolsInfo(false)
+    }
+
     return (
         <section className={styles.container}>
-            <span className={styles.title}>Workshops</span>
+             <div className={styles.titlecontainer}>
+                <img className={styles.hammer} src={Hammer}/> 
+                <span className={styles.title}>Workshops</span>
+                <img className={styles.hammertwo} src={Hammer}/> 
+            </div>
             <div className={styles.workshopSectionContainer}>
-                <div className={styles.workshopContainer}>
-                    <img className={styles.workshopIcon} />
+                <div className={styles.workshopContainer}>  
+                    <button className={styles.workshopIcon} onClick={handleClick}/>
                     <div className={styles.workshopDescContainer}>
                         <span className={styles.workshopTitle}>Workshop Name</span>
                         <span className={styles.workshopDesc}> Important Workshop Fact 1</span>
@@ -15,7 +34,7 @@ const GameJamWorkshops = () => {
                     </div>
                 </div>
                 <div className={styles.workshopContainer}>
-                    <img className={styles.workshopIcon} />
+                    <button className={styles.workshopIcon} onClick={handleClick}/>
                     <div className={styles.workshopDescContainer}>
                         <span className={styles.workshopTitle}>Workshop Name</span>
                         <span className={styles.workshopDesc}> Important Workshop Fact 1</span>
@@ -24,7 +43,7 @@ const GameJamWorkshops = () => {
                     </div>
                 </div>
                 <div className={styles.workshopContainer}>
-                    <img className={styles.workshopIcon} />
+                <button className={styles.workshopIcon} onClick={handleClick}/>
                     <div className={styles.workshopDescContainer}>
                         <span className={styles.workshopTitle}>Workshop Name</span>
                         <span className={styles.workshopDesc}> Important Workshop Fact 1</span>

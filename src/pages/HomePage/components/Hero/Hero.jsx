6022dcc5768d7327df469a6c7acd6a54c84dfd9a
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-04-02 13:25:21 -0700

    update styles for home page

--- src/pages/HomePage/components/Hero/Hero.jsx
+++ src/pages/HomePage/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import styles from "./hero.module.css"
 const Hero = () => {
     return (
         <section className={styles.hero}>
-            <h1>MercedJam 2025</h1>
+            <h1>UC Merced GDC Game Jam 2025</h1>
             <p>Starts in HH:MM:SS</p>
         </section>
     );

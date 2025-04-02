05af76a2d7eb496b1f1cfc8970618fce1358918e
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-04-02 13:36:01 -0700

    add comment about timestamp in hero

--- src/pages/HomePage/components/Hero/Hero.jsx
+++ src/pages/HomePage/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
         <section className={styles.hero}>
             <h1>UC Merced GDC Game Jam 2025</h1>
             <p>Starts in HH:MM:SS</p>
+            {/* Starts at timestamp 1744426800 */}
         </section>
     );
 };
